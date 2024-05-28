d370099f884eccdb15677d3f0fd2d0df367ae3eb
cdtor <cartermosby12@gmail.com>
2024-05-28 16:24:23 -0600

    Dashboard integration

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
@@ -22,6 +23,7 @@ function App() {
             <Route path='/' element={ <Home/> } />
             <Route path='/login' element={ <Login/> } />
             <Route path='/register' element={ <Register/> } />
+            <Route path='/dashboard' element={ <Dashboard/> } />
           </Routes>
     </UserContextProvider>
   );
--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -22,7 +22,7 @@ export default function Login() {
           toast.error(data.error)
         } else {
           setData({});
-          navigate('/');
+          navigate('/Dashboard');
         }
       } catch (error) {
         
